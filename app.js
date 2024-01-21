9abc7ba7f799ea0b65bf1f5ba2c6e6ed7ab5c891
Secharo <157318087+Secharo@users.noreply.github.com>
2024-01-21 15:21:58 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,76 @@
+let numeroSecreto = 0;
+let intentos = 0;
+let numerosSorteados = [];
+let numeroMaximo = 5;
+
+    function verificarIntento() {
+        let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+        
+        
+        if(numeroDeUsuario===numeroSecreto){
+            asignarTextoElemento('p',`¡Lo lograste! en ${intentos} ${(intentos == 1) ? 'intento.' : 'intentos.'}`);
+            
+            document.getElementById('reiniciar').removeAttribute('disabled');
+            
+        } else{
+            if(numeroDeUsuario<numeroSecreto){
+                asignarTextoElemento('p','El número es mayor.');
+            }else{
+                
+                asignarTextoElemento('p','El número es menor.');
+                
+                
+            }
+            
+            intentos ++;
+            limpiarCaja();
+        }
+        return  
+    }
+
+function condicionesIniciales(){
+    asignarTextoElemento('h1','Juego del número secreto.');
+    asignarTextoElemento('p',`Ingresas un número del 1 a ${numeroMaximo}`);
+    intentos =1;
+    numeroSecreto = generarNumeroSecreto();
+   
+}
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return
+}
+    function generarNumeroSecreto() { 
+        let numeroGenerado = parseInt(Math.floor(Math.random()*numeroMaximo +1));
+
+        
+
+        if(numerosSorteados.length == numeroMaximo){
+
+            asignarTextoElemento('p','Ya jugaste todos los numeros posibles');
+            
+        } else {
+            
+            
+            if (numerosSorteados.includes(numeroGenerado)){
+                return generarNumeroSecreto();
+            }else{
+                numerosSorteados.push(numeroGenerado);
+                return numeroGenerado;
+            }
+        }
+        }
+    
+    function limpiarCaja(){
+        document.querySelector('#valorUsuario').value = '';
+}
+
+function reiniciarJuego(){
+    limpiarCaja();
+    condicionesIniciales();   
+    document.querySelector('#reiniciar').setAttribute('disabled','true');
+}
+
+condicionesIniciales();
+  
