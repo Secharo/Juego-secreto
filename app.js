a0b6000a2e543ca1d7c919f89254b717e29b040f
Sebastian Charria <157318087+Secharo@users.noreply.github.com>
2024-01-21 16:07:08 -0500

    Add files via upload
    Version 1.2.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let numeroSecreto = 0;
 let intentos = 0;
 let numerosSorteados = [];
-let numeroMaximo = 5;
+let numeroMaximo = 10;
 
     function verificarIntento() {
         let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
